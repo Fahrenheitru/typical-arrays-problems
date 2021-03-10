ea3488dfc59fa2285e43fd5b96a9839cc81b8172
Fahrenheitru <ifahrenheitru@gmail.com>
2021-03-10 20:36:19 +0500

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) { 
+    return 0;
+  } else if (array.length != 0){
+    let minArr = array[0];
+    for (let i = 0; i < array.length; i++) {
+    if (minArr >= array[i]) {
+      minArr = array[i];
+    }
+  }
+  return minArr;
+  }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else if (array.length != 0) {
+    let maxArr = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maxArr <= array[i]) {
+        maxArr = array[i];
+      }
+    }
+    return maxArr;
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else if (array.length != 0) {
+    let avgArr;
+    for (let i = 0; i < array.length; i++) {
+      avgArr = array.reduce((sum,current)=>sum+current,0);
+    }
+    return avgArr/(array.length);
+  }
+};
